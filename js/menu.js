cae144c600b85c3a690ca3247bded5bd022b1a06
Gaurav Narayan Mathur <107292537+gnm-personal@users.noreply.github.com>
2025-12-14 16:10:36 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -67,6 +67,7 @@ fetch("header.html")
 
           	// if toggle button not present, create it (only visible on mobile via CSS)
           	if (!li.querySelector(".submenu-toggle")) {
+				if(window.screensize <=576){
 	            const toggle = document.createElement("button");
 	            toggle.className = "submenu-toggle";
 	            toggle.setAttribute("aria-expanded", "false");
@@ -93,6 +94,7 @@ fetch("header.html")
 	                	if (t) t.setAttribute("aria-expanded", isOpen ? "true" : "false");
 	              	}
 	            });
+				}
 	        }
     	}
     });
