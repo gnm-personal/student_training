f78c9fe92e1b4a739b981308898208b83576eee6
Gaurav Narayan Mathur <107292537+gnm-personal@users.noreply.github.com>
2026-01-03 10:28:42 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -64,7 +64,7 @@ fetch("header.html")
 							ev.stopPropagation();
 							menuList.forEach(s_li =>{
 								const s_submenuExists = s_li.querySelector(".submenu");
-								if(s_submenuExists){
+								if(s_submenuExists && s_li !== li){
 									console.log(s_submenuExists);
 									console.log(s_li);
 									s_li.classList.remove("open-submenu");
