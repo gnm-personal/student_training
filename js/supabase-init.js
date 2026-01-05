401b0ce997777a8e4a78f020b01fa6f5c0c96ade
Gaurav Narayan Mathur <107292537+gnm-personal@users.noreply.github.com>
2026-01-05 11:05:41 +0530

    Update supabase-init.js

--- js/supabase-init.js
+++ js/supabase-init.js
@@ -1,13 +1,13 @@
-const SUPABASE_URL = "https://duvpeufsifgjgynfwqxp.supabase.co";   // your project url
-const SUPABASE_ANON_KEY = "sb_publishable_CiKs3Kg-0CTpgCpg4cJ5yQ_x4V8lWqd";
+// const SUPABASE_URL = "https://duvpeufsifgjgynfwqxp.supabase.co";   // your project url
+// const SUPABASE_ANON_KEY = "sb_publishable_CiKs3Kg-0CTpgCpg4cJ5yQ_x4V8lWqd";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+const supabaseClient = window.supabase.createClient(
+  "https://duvpeufsifgjgynfwqxp.supabase.co",
+  "sb_publishable_CiKs3Kg-0CTpgCpg4cJ5yQ_x4V8lWqd"
 );
 
 if (supabase) {
-  console.log("Database connected:", supabase);
+  console.log("Database connected:", supabaseClient);
 } else {
   console.log("Database not connected");
 }
