310830af593ec52255b38e14ad577530ce9b17f0
Gaurav Narayan Mathur <107292537+gnm-personal@users.noreply.github.com>
2026-01-14 21:04:47 +0530

    Update supabase-init.js

--- js/supabase-init.js
+++ js/supabase-init.js
@@ -1,10 +1,26 @@
-const supabaseClient = window.supabase.createClient(
-  "https://duvpeufsifgjgynfwqxp.supabase.co",
-  "sb_publishable_CiKs3Kg-0CTpgCpg4cJ5yQ_x4V8lWqd"
-);
+// const supabaseClient = window.supabase.createClient(
+//   "https://duvpeufsifgjgynfwqxp.supabase.co",
+//   "sb_publishable_CiKs3Kg-0CTpgCpg4cJ5yQ_x4V8lWqd"
+// );
 
-if (supabaseClient) {
-  console.log("Database connected:", supabaseClient);
-} else {
-  console.log("Database not connected");
-}
+// if (supabaseClient) {
+//   console.log("Database connected:", supabaseClient);
+// } else {
+//   console.log("Database not connected");
+// }
+
+fetch("./data.json")
+  .then(res => res.json())
+  .then(config => {
+    const supabaseClient = window.supabase.createClient(
+      config.supabase_url,
+      config.supabase_key
+    );
+
+    if (supabaseClient) {
+      console.log("Database connected:", supabaseClient);
+    } else {
+      console.log("Database not connected");
+    }
+  })
+  .catch(err => console.error(err));
